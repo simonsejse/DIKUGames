5594e02a3fbfebf52f9eb70f895c11a3d3b15f36
Alexander Christensen <alex_c007@hotmail.com>
2021-02-21 20:58:56 +0100

    Initial setup for Galaga

---
+++ Galaga/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Galaga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var game = new Game();
+            // game.Run();
+            Console.WriteLine("Hello World!"); // TODO: Delete this line!
+        }
+    }
+}
