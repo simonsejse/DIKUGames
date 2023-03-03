0e21731387be8c27910619b2a693344f8226d2b1
Simon Winther <simonwinther@MacBook-Pro-tilhrende-Simon.local>
2023-03-03 14:19:04 +0100

    Added ProcessEvent()

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -13,9 +13,9 @@ namespace Galaga
     public class Game : DIKUGame, IGameEventProcessor
     {
         private GameEventBus eventBus;
-        private Player _player;
+        private Player player;
         private Score score = new Score();
-        private const int EXPLOSION_LENGTH_MS = 500;
+        private static int EXPLOSION_LENGTH_MS = 500;
         private static int numEnemies = 8;
         /* Images */
         private List<Image> explosionStrides;
@@ -29,7 +29,7 @@ namespace Galaga
         public Game(WindowArgs windowArgs) : base(windowArgs)
         {
             eventBus = new GameEventBus();
-            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent, GameEventType.WindowEvent });
             window.SetKeyEventHandler(KeyHandler);
             eventBus.Subscribe(GameEventType.InputEvent, this);
             SpawnEnemies();
@@ -49,7 +49,7 @@ namespace Galaga
            explosionStrides = ImageStride.CreateStrides(8, Path.Combine("Assets", "Images", "Explosion.png"));
         }
         private void SetupPlayer(){
-            _player = new Player(
+            player = new Player(
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
                 new Image(Path.Combine("Assets", "Images", "Player.png"))
             );
@@ -104,28 +104,32 @@ namespace Galaga
             switch (key)
             {
                case KeyboardKey.Escape:
-                    this.window.CloseWindow();
+                    GameEvent close = new GameEvent {
+                        To = this,
+                        EventType = GameEventType.WindowEvent
+                    };
+                    eventBus.RegisterEvent(close);
                     break;
                 case KeyboardKey.R:
                     SpawnEnemies();
                     break;
                 case KeyboardKey.Space:
-                    Vec2F pos = _player.GetPosition();
+                    Vec2F pos = player.GetPosition();
                     playerShots.AddEntity(
-                        new PlayerShot(new Vec2F(pos.X + _player.GetExtent().X / 2, pos.Y + _player.GetExtent().Y / 2), new Image(Path.Combine("Assets", "Images", "BulletRed2.png")))
+                        new PlayerShot(new Vec2F(pos.X + player.GetExtent().X / 2, pos.Y + player.GetExtent().Y / 2), new Image(Path.Combine("Assets", "Images", "BulletRed2.png")))
                     );
                     break;
                 case KeyboardKey.W:
-                    _player.SetMoveUp(true);
+                    player.SetMoveUp(true);
                     break;
                 case KeyboardKey.A:
-                    _player.SetMoveLeft(true);
+                    player.SetMoveLeft(true);
                     break;
                 case KeyboardKey.S:
-                    _player.SetMoveDown(true);
+                    player.SetMoveDown(true);
                     break;
                 case KeyboardKey.D:
-                    _player.SetMoveRight(true);
+                    player.SetMoveRight(true);
                     break;
             }
             // TODO: Close window if escape is pressed
@@ -136,16 +140,16 @@ namespace Galaga
             switch (key)
             {
                 case KeyboardKey.W:
-                    _player.SetMoveUp(false);
+                    player.SetMoveUp(false);
                     break;
                 case KeyboardKey.A:
-                    _player.SetMoveLeft(false);
+                    player.SetMoveLeft(false);
                     break;
                 case KeyboardKey.S:
-                    _player.SetMoveDown(false);
+                    player.SetMoveDown(false);
                     break;
                 case KeyboardKey.D:
-                    _player.SetMoveRight(false);
+                    player.SetMoveRight(false);
                     break;
 
             }
@@ -163,12 +167,14 @@ namespace Galaga
         }
         public void ProcessEvent(GameEvent gameEvent)
         {
-            // Leave this empty for now
+            if (gameEvent.EventType == GameEventType.WindowEvent){
+                window.CloseWindow();
+            }
         }
 
         public override void Render()
         {
-            _player.Render();
+            player.Render();
             enemies.RenderEntities();
             playerShots.RenderEntities();
             enemyExplosions.RenderAnimations();
@@ -179,7 +185,7 @@ namespace Galaga
         {
             window.PollEvents();
             eventBus.ProcessEventsSequentially();
-            _player.Move();
+            player.Move();
             IterateShots();
             scoreText.SetText(String.Format("Score: {0}", score.GetScore()));
         }
--- Galaga/entities/Player-MacBook Pro tilhørende Simon.cs
+++ Galaga/entities/Player-MacBook Pro tilhørende Simon.cs
@@ -6,8 +6,8 @@ namespace Galaga
 {
     public class Player
     {
-        private Entity entity;
-        private DynamicShape shape;
+        private Entity _entity;
+        private DynamicShape _shape;
         private float _moveUp = 0.0f;
         private float _moveDown = 0.0f;
         private float _moveLeft = 0.0f;
@@ -16,40 +16,40 @@ namespace Galaga
 
         public Player(DynamicShape shape, IBaseImage image)
         {
-            this.entity = new Entity(shape, image);
-            this.shape = shape;
+            this._entity = new Entity(shape, image);
+            this._shape = shape;
         }
 
         public Vec2F GetExtent(){
-            return shape.Extent;
+            return _shape.Extent;
         }
         public void Render()
         {
-            this.entity.RenderEntity();
+            this._entity.RenderEntity();
         }
         private void UpdateDirection()
         {
             float x = _moveLeft + _moveRight;
             float y = _moveUp + _moveDown;
-            shape.Direction = new DIKUArcade.Math.Vec2F(x, y);
+            _shape.Direction = new DIKUArcade.Math.Vec2F(x, y);
         }
 
         public void Move()
         {
             // TODO: move the shape and guard against the window borders
-            if (shape.Position.X < 0){
+            if (_shape.Position.X < 0){
                 SetMoveLeft(false);
             }
-            if (shape.Position.X > 0.9){
+            if (_shape.Position.X > 0.9){
                 SetMoveRight(false);
             }
-            if (shape.Position.Y > 0.6){
+            if (_shape.Position.Y > 0.6){
                 SetMoveUp(false); 
             } 
-            if (shape.Position.Y < 0){
+            if (_shape.Position.Y < 0){
                 SetMoveDown(false); 
             }
-            this.shape.Move();
+            this._shape.Move();
         }
 
         public void SetMoveUp(bool val)
@@ -76,7 +76,7 @@ namespace Galaga
             UpdateDirection();
         }
         public Vec2F GetPosition(){
-            return shape.Position;
+            return _shape.Position;
         }
     }
 }
