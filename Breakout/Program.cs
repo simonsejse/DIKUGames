d2888395fbfaf979fe793d4447e0b39abd02a198
simonwinther <simonwa01@gmail.com>
2023-04-25 17:02:53 +0200

    Project

--- Breakout/Program.cs
+++ Breakout/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Application;
+
+
+public class Application
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
