747c5b7b474c709a6d1edf1555c7567d5a6a06d3
simonwinther <simonwa01@gmail.com>
2023-04-25 16:52:45 +0200

    added new project

---
+++ BreakoutTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BreakoutTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
