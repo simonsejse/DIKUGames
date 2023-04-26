12402c8af115b247f1c7fa37d6938876f68d3d65
SimonWin <simonwa01@gmail.com>
2023-04-26 22:22:45 +0200

    til jarl

--- Breakout/Entities/PlayerEntity.cs
+++ Breakout/Entities/PlayerEntity.cs
@@ -6,44 +6,47 @@ using DIKUArcade.Math;
 
 namespace Breakout.Entites;
 
-
+/// <summary>
+/// A class that represents the player entity.
+/// </summary>
 public class PlayerEntity : Entity
 {
-    private float _moveLeft = 0.0f;
-    private float _moveRight = 0.0f;
-    private float _movementSpeed = 0.03f;
+    private float _moveLeft;
+    private float _moveRight;
+    private const float MovementSpeed = 0.025f;
     
     public PlayerEntity(Shape shape, IBaseImage image) : base(shape, image)
     {
 
     }
 
-    private void UpdateDirection()
-    {
-        var x = _moveLeft + _moveRight;
-        Shape.AsDynamicShape().Direction = new Vec2F(x, 0);
-    }
 
     public void Move()
     {
         if (Shape.Position.X < 0){
             SetMoveLeft(false);
         }
-        if (Shape.Position.X > 0.9){
+        if (Shape.Position.X > 1.0f - 0.2f){
             SetMoveRight(false);
         }
         Shape.Move();
     }
+    
+    private void UpdateDirection()
+    {
+        var x = _moveLeft + _moveRight;
+        Shape.AsDynamicShape().Direction = new Vec2F(x, 0);
+    }
 
     public void SetMoveLeft(bool val)
     {
-        _moveLeft = val ? _moveLeft - _movementSpeed : 0f;
+        _moveLeft = val ? _moveLeft - MovementSpeed : 0f;
         UpdateDirection();
     }
 
     public void SetMoveRight(bool val)
     {
-        _moveRight = val ? _moveRight + _movementSpeed : 0f;
+        _moveRight = val ? _moveRight + MovementSpeed : 0f;
         UpdateDirection();
     }
 
