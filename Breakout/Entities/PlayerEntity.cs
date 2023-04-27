9d7ab10a55dcfef39fc8d9c9c1f8b661f11d69a5
jarljarl <124596183+jarljarl@users.noreply.github.com>
2023-04-27 18:06:41 +0100

    playerentity
    Comments og kant fix

--- Breakout/Entities/PlayerEntity.cs
+++ Breakout/Entities/PlayerEntity.cs
@@ -11,43 +11,51 @@ namespace Breakout.Entites;
 /// </summary>
 public class PlayerEntity : Entity
 {
+    #region Properties and fields
+    // Field for player's movement and its movement speed
     private float _moveRight = 0.0f;
     private float _moveLeft = 0.0f;
-    private const float MovementSpeed = 0.025f;
+    private const float MovementSpeed = 0.05f;
+    #endregion
     
+    #region Constructor
+    // Constructor that intializes player's shape and image by using the base class constructor
     public PlayerEntity(Shape shape, IBaseImage image) : base(shape, image)
     {
 
     }
+    #endregion
 
-
+    #region Methods
+    // Moves the player left or right based upon their movement fields and speed and updates their position and direction
+    // Also restricts position to be within game window limits
     public void Move()
     {
         if (Shape.Position.X < 0f){
             SetMoveLeft(false);
         }
-        if (Shape.Position.X > 1.0f - 0.2f){
+        if (Shape.Position.X > (1.0f - Shape.Extent.X)){
             SetMoveRight(false);
         }
         Shape.Move();
     }
-    
+    // Updates the player's direction based upon their movement fields
     private void UpdateDirection()
     {
         var x = _moveLeft + _moveRight;
         Shape.AsDynamicShape().Direction = new Vec2F(x, 0);
     }
-
+    // Sets the movement of the player to Left based upon a boolean value and updates the direction
     public void SetMoveLeft(bool val)
     {
         _moveLeft = val ? _moveLeft - MovementSpeed : 0f;
         UpdateDirection();
     }
-
+    // Sets the movement of the player to Right based upon a boolean value and updates the direction
     public void SetMoveRight(bool val)
     {
         _moveRight = val ? _moveRight + MovementSpeed : 0f;
         UpdateDirection();
     }
-
+    #endregion
 }
