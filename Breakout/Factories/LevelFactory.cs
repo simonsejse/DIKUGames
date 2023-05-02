8cab0673f8161e04eff4bcf2905e2284ee0ee806
simonwinther <simonwa01@gmail.com>
2023-05-02 16:19:33 +0200

    Made it environment compatible

--- Breakout/Factories/LevelFactory.cs
+++ Breakout/Factories/LevelFactory.cs
@@ -14,10 +14,11 @@ public class LevelFactory : IModelFactory<Level>
     /// <returns>A new instance of <see cref="Level"/> with the parsed data.</returns>
     public Level Parse(string data)
     {
+        
         //TODO: Change this to LINQ as well
         var mapStart = data.Split("Map:")[1];
         var mapEnd = mapStart.Split("Map/")[0];
-        var map = mapEnd.Split("\r\n")
+        var map = mapEnd.Split( Environment.NewLine)
             .Select(row => row.Trim())
             .Where(row => row.Length > 0)
             .ToArray()
@@ -28,7 +29,7 @@ public class LevelFactory : IModelFactory<Level>
         
         var metaStart = data.Split("Meta:")[1];
         var metaEnd = metaStart.Split("Meta/")[0];
-        var metadata = metaEnd.Split("\r\n")
+        var metadata = metaEnd.Split(Environment.NewLine)
             .Select(line => line.Trim())
             .Where(line => line.Length > 0)
             .ToDictionary(line => line.Split(":")[0].Trim(), line => line.Split(":")[1].Trim());
@@ -42,9 +43,10 @@ public class LevelFactory : IModelFactory<Level>
         
         var meta = new Meta(name, time, hardened, powerUp, unbreakable);
  
-        var legendStart = data.Split("Legend:")[1];
-        var legendEnd = legendStart.Split("Legend/")[0];
-        var legend = legendEnd.Split("\r\n")
+        string legendStart = data.Split("Legend:")[1];
+        string legendEnd = legendStart.Split("Legend/")[0];
+        
+        Dictionary<char, string> legend = legendEnd.Split(Environment.NewLine)
             .Select(line => line.Trim())
             .Where(line => line.Length > 0)
             .ToDictionary(line => line[0], line => line.Split(")")[1].Trim());
