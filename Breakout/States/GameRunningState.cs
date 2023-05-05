63314e56c6fc1a01ba0bc9e58a196974410b7cdf
Jarl <magnus@krugstrup.dk>
2023-05-06 00:20:58 +0200

    Fjernet commented ud kode

--- Breakout/States/GameRunningState.cs
+++ Breakout/States/GameRunningState.cs
@@ -78,7 +78,6 @@ public class GameRunningState : IGameState
     public void RenderState()
     {
         _playerEntity.RenderEntity();
-        //_ballEntity.RenderEntity();
         _entityContainers.RenderEntities();
     }
 
