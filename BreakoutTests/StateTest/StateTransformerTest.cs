c00a8030732c95976ef43ca5b884e2837113efb9
SimonWin <simonwa01@gmail.com>
2023-05-05 22:28:19 +0200

    Added new statetransformer test

---
+++ BreakoutTests/StateTest/StateTransformerTest.cs
@@ -0,0 +1,39 @@
+﻿using Breakout.States;
+
+namespace BreakoutTests.StateTest;
+
+[TestFixture]
+public class StateTransformerTest
+{
+
+    private StateTransformer _transformer;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _transformer = new StateTransformer();
+    }
+    
+    [Test]
+    public void TestTransformStringToState()
+    {
+        var transformStringToState = _transformer.TransformStringToState(nameof(GameState.Menu));
+        Assert.Multiple(() =>
+        {
+            Assert.That(transformStringToState, Is.EqualTo(GameState.Menu));
+            Assert.That(() => _transformer.TransformStringToState("Barry"), Throws.ArgumentException);
+        });
+    }
+    
+    [Test]
+    public void TestTransformStateToString()
+    {
+        string transformStateToString = _transformer.TransformStateToString(GameState.Menu);
+        Assert.Multiple(() =>
+        {
+            Assert.That(transformStateToString, Is.EqualTo(nameof(GameState.Menu)));
+            Assert.That(() => _transformer.TransformStateToString((GameState) 5), Throws.ArgumentException);
+        });    
+    }
+    
+}
