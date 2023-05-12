8d475f3cf39d9c961c5c5871eaad3da6a8de1049
Jarl <magnus@krugstrup.dk>
2023-05-12 13:42:29 +0200

    Moveplayer tests

--- BreakoutTests/CommandsTest/MovePlayer.cs
+++ BreakoutTests/CommandsTest/MovePlayer.cs
@@ -1,7 +1,6 @@
 ﻿using Breakout.Commands.GameRunning;
 using Breakout.Entities;
 using DIKUArcade.GUI;
-using NUnit.Framework;
 
 namespace BreakoutTests.Commands.GameRunning
 {
@@ -16,60 +15,48 @@ namespace BreakoutTests.Commands.GameRunning
         [Test]
         public void MoveLeftTrueTest()
         {
-            // Arrange
             PlayerEntity playerEntity = PlayerEntity.Create();
             var shouldMove = true;
             var command = new MovePlayerLeftCommand(playerEntity, shouldMove);
-
-            // Act
+            
             command.Execute();
-
-            // Assert
+            
             Assert.AreEqual(shouldMove, playerEntity.GetMoveLeft());
         }
 
         [Test]
         public void MoveLeftFalseTest()
         {
-            // Arrange
             PlayerEntity playerEntity = PlayerEntity.Create();
             var shouldMove = false;
             var command = new MovePlayerLeftCommand(playerEntity, shouldMove);
-
-            // Act
+            
             command.Execute();
-
-            // Assert
+            
             Assert.AreEqual(shouldMove, playerEntity.GetMoveLeft());
         }
         
         [Test]
         public void MoveRightTrueTest()
         {
-            // Arrange
             PlayerEntity playerEntity = PlayerEntity.Create();
             var shouldMove = true;
             var command = new MovePlayerLeftCommand(playerEntity, shouldMove);
-
-            // Act
+            
             command.Execute();
-
-            // Assert
+            
             Assert.AreEqual(shouldMove, playerEntity.GetMoveLeft());
         }
 
         [Test]
         public void MoveRightFalseTest()
         {
-            // Arrange
             PlayerEntity playerEntity = PlayerEntity.Create();
             var shouldMove = false;
             var command = new MovePlayerRightCommand(playerEntity, shouldMove);
-
-            // Act
+            
             command.Execute();
-
-            // Assert
+            
             Assert.AreEqual(shouldMove, playerEntity.GetMoveRight());
         }
     }
