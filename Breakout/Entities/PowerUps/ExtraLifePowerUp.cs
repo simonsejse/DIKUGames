21abc06abdbf2cc460fc7bb5c1ed32b1ae857d79
SimonWin <simonwa01@gmail.com>
2023-05-19 13:56:38 +0200

    cdadc

--- Breakout/Entities/PowerUps/ExtraLifePowerUp.cs
+++ Breakout/Entities/PowerUps/ExtraLifePowerUp.cs
@@ -14,8 +14,8 @@ public class ExtraLifePowerUp : IPowerUpType
 
     public void DropPowerUp(BlockEntity block)
     {
-        float positionY = block.Shape.Position.Y;
-        float positionX = block.Shape.Position.X;
+        float positionY = block.Shape.Position.Y - ConstantsUtil.PowerUpExtent.Y;
+        float positionX = block.Shape.Position.X + (block.Shape.Extent.X / 2) - ConstantsUtil.PowerUpExtent.X / 2;
 
         var position = new Vec2F(positionX, positionY);
         EntityManager.PowerUps.AddEntity(PowerUpEntity.Create(position, "LifePickUp"));
