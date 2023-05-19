9da45410eea4a9c30ac00b483fb0d7571ab4f8f8
SimonWin <simonwa01@gmail.com>
2023-05-19 13:54:14 +0200

    dsadas

--- Breakout/Entities/PowerUps/ExtraLifePowerUp.cs
+++ Breakout/Entities/PowerUps/ExtraLifePowerUp.cs
@@ -1,4 +1,5 @@
 ﻿using Breakout.Containers;
+using Breakout.Utility;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 
@@ -13,8 +14,9 @@ public class ExtraLifePowerUp : IPowerUpType
 
     public void DropPowerUp(BlockEntity block)
     {
-        float positionY = block.Shape.Position.Y;
-        float positionX = block.Shape.Position.X;
+        float positionY = block.Shape.Position.Y + ConstantsUtil.BlockExtent.Y / 2f;
+        float positionX = block.Shape.Position.X + ConstantsUtil.BlockExtent.X / 2f;
+
         var position = new Vec2F(positionX, positionY);
         EntityManager.PowerUps.AddEntity(PowerUpEntity.Create(position, "LifePickUp"));
     }
