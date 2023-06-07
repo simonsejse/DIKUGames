916e740b4fe801407a72c78b6c31a7e97badcbb1
Victorschimmell <Victorschimmell@gmail.com>
2023-06-07 12:37:49 +0200

    vf

---
+++ Breakout/Entities/BlockBehaviors/IBlockTypeBehavior.cs
@@ -0,0 +1,10 @@
+﻿namespace Breakout.Entities.BlockBehaviors;
+
+public interface IBlockTypeBehavior {
+    /// <summary>
+    /// Modifies the health of the block based on its type.
+    /// </summary>
+    /// <param name="health">The current health of the block.</param>
+    /// <returns>The modified health of the block.</returns>
+    int ModifyHealth(int health);
+}
